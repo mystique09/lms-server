06e97eda803fd7ed443d8600aab931c63241ff99
mystique09 <knightkrypto7@gmail.com>
2022-06-11 13:50:09 +0800

    add: class_work queries

--- database/queries/class_work.sql
+++ database/queries/class_work.sql
@@ -1,4 +1,4 @@
---name: GetClassWork :one
+-- name: GetClassWork :one
 --description: Get a class work by id
 --parameters: id
 --returns: class_work
@@ -6,7 +6,7 @@ SELECT *
 FROM class_work 
 WHERE id = $1;
 
---name: ListClassWork :many
+-- name: ListClassWork :many
 --description: List all class works
 --parameters: none
 --returns: class_work
