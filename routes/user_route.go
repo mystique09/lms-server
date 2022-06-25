136fcaa740acc46591b3f694f89d531aef5c292c
mystique09 <knightkrypto7@gmail.com>
2022-06-26 01:29:23 +0800

    add: initial routes for users

--- routes/user_route.go
+++ routes/user_route.go
@@ -2,20 +2,107 @@ package routes
 
 import (
 	"context"
+	"net/http"
+	database "server/database/sqlc"
+	"server/utils"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
+type UserUpdateDTO struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 /*
-A GetUser method for rt struct to retrive all users.
+A function to retrive all users.
 */
 func (rt *Route) GetUsers(c echo.Context) error {
-	ctx := context.Background()
-	users, err := rt.DB.GetUsers(ctx)
+	users, err := rt.DB.GetUsers(rt.CTX)
 
 	if err != nil {
-		return c.JSON(500, err)
+		return c.JSON(http.StatusInternalServerError, utils.NewResponse(0, nil, err.Error()))
 	}
 
-	return c.JSON(200, users)
+	return c.JSON(http.StatusOK, utils.NewResponse(1, users, ""))
+}
+
+/*
+A function to retrive a user by ID.
+*/
+func (rt *Route) GetUser(c echo.Context) error {
+	ctx := context.Background()
+	id := c.Param("id")
+	uid, err := uuid.Parse(id)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, err.Error()))
+	}
+
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, "Please provide an ID.")
+	}
+
+	user, err := rt.DB.GetUser(ctx, uid)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, utils.NewResponse(1, user, ""))
+}
+
+/*
+A function to update a user by ID.
+*/
+func (rt *Route) UpdateUser(c echo.Context) error {
+	ctx := context.Background()
+	id := c.Param("id")
+	uid, err := uuid.Parse(id)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, err.Error()))
+	}
+
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, "Please provide an ID."))
+	}
+
+	user := new(database.UpdateUserParams)
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, err.Error()))
+	}
+
+	user.ID = uid
+
+	if err := rt.DB.UpdateUser(ctx, *user); err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, utils.NewResponse(1, user, ""))
+}
+
+/*
+A function to delete a user by ID.
+*/
+func (rt *Route) DeleteUser(c echo.Context) error {
+	ctx := context.Background()
+	id := c.Param("id")
+	uid, err := uuid.Parse(id)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, err.Error()))
+	}
+
+	if id == "" {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, "Please provide an ID."))
+	}
+
+	if err := rt.DB.DeleteUser(ctx, uid); err != nil {
+		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, nil, err.Error()))
+	}
+
+	return c.JSON(http.StatusOK, utils.NewResponse(1, id, ""))
 }
