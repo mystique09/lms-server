df1b609133fc37be5e1d275c44e0b7f9affe8d66
mystique09 <knightkrypto7@gmail.com>
2022-06-26 14:51:16 +0800

    feat: implement jwt middleware in users route

--- main.go
+++ main.go
@@ -44,13 +44,13 @@ func main() {
 
 	server := echo.New()
 	server.Use(routes.LoggerMiddleware())
-	server.Use(routes.RateLimitMiddleware())
+	server.Use(routes.RateLimitMiddleware(20))
 	server.Use(routes.CorsMiddleware())
 
 	server.GET("/api/v1", indexHandler)
 	server.POST("/api/v1/auth", rt.CreateUser)
 
-	user_route := server.Group("/api/v1/users")
+	user_route := server.Group("/api/v1/users", routes.JwtAuthMiddleware())
 	{
 		user_route.GET("", rt.GetUsers)
 		user_route.GET("/:id", rt.GetUser)
