9f6be9fc6048a5bf44ea762691b0f39a2c196946
mystique09 <knightkrypto7@gmail.com>
2022-06-26 15:45:16 +0800

    add: utility functions for creating new jwt token

--- utils/jwt_util.go
+++ utils/jwt_util.go
@@ -17,10 +17,26 @@ type (
 	}
 )
 
-func NewJwtClaims(username, email, role string) *JwtClaims {
-	return &JwtClaims{
+func NewJwtPayload(username, email, role string) JwtUserPayload {
+	return JwtUserPayload{
 		Username: username,
 		Email:    email,
 		Role:     role,
 	}
+}
+
+func NewJwtClaims(payload JwtUserPayload) JwtClaims {
+	return JwtClaims{
+		Username: payload.Username,
+		Email:    payload.Email,
+		Role:     payload.Role,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: 0,
+		},
+	}
+}
+
+func NewJwtToken(payload JwtClaims, secret_key []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return token.SignedString(secret_key)
 }
