6e2079f89ebdc4f075b84f480d65d9053b3e9df9
mystique09 <knightkrypto7@gmail.com>
2022-06-26 14:39:18 +0800

    add: middlewares

--- main.go
+++ main.go
@@ -43,6 +43,8 @@ func main() {
 	}
 
 	server := echo.New()
+	server.Use(routes.LoggerMiddleware())
+	server.Use(routes.RateLimitMiddleware())
 	server.GET("/api/v1", indexHandler)
 	server.POST("/api/v1/auth", rt.CreateUser)
 
