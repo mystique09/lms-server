146363fbf1c6f539b1edbe3756982a60c13c51df
mystique09 <knightkrypto7@gmail.com>
2022-07-16 06:08:05 +0800

    add: query of all submitted assignments, and query all assignments(admin only)

--- database/queries/class_work.sql
+++ database/queries/class_work.sql
@@ -4,11 +4,24 @@
 --returns: class_work
 SELECT * 
 FROM class_work 
-WHERE id = $1;
+WHERE id = $1, class_id = $2;
 
--- name: ListClassWork :many
+-- name: ListClassworkAdmin :many
 --description: List all class works
 --parameters: none
 --returns: class_work
 SELECT *
-FROM class_work;
+FROM class_work
+WHERE class_id = $1
+ORDER BY created_at
+DESC;
+
+-- name: ListSubmittedClassworks :many
+--description: List all submitted classworks of a user
+--parameters: user_id, class_id
+--returns: class_work
+SELECT *
+FROM class_work
+WHERE class_id = $1, user_id = $2
+ORDER BY created_at
+DESC;
