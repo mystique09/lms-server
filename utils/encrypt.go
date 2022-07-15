a9c59ddaa541569b96e636bf430708f7cbb059ee
mystique09 <knightkrypto7@gmail.com>
2022-07-15 15:59:29 +0800

    add: encrypt function

---
+++ utils/encrypt.go
@@ -0,0 +1,12 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func Encrypt(plain_text string) (string, error) {
+	hashed_password, err := bcrypt.GenerateFromPassword([]byte(plain_text), bcrypt.DefaultCost)
+
+	if err != nil {
+		return plain_text, err
+	}
+	return string(hashed_password), nil
+}
