b43e72cc1bb838a3f642fb216f994754fca956d7
mystique09 <knightkrypto7@gmail.com>
2022-07-16 06:46:47 +0800

    feat: only the user's classwork can be updated, get, deleted

--- database/queries/class_work.sql
+++ database/queries/class_work.sql
@@ -3,16 +3,16 @@
 --parameters: id
 --returns: class_work
 SELECT * 
-FROM class_work 
-WHERE id = $1, class_id = $2;
+FROM "class_work" 
+WHERE id = $1, user_id = $2, class_id = $3;
 
 -- name: ListClassworkAdmin :many
 --description: List all class works
 --parameters: none
 --returns: class_work
 SELECT *
-FROM class_work
-WHERE class_id = $1
+FROM "class_work"
+WHERE class_id = $2
 ORDER BY created_at
 DESC;
 
@@ -21,7 +21,23 @@ DESC;
 --parameters: user_id, class_id
 --returns: class_work
 SELECT *
-FROM class_work
+FROM "class_work"
 WHERE class_id = $1, user_id = $2
 ORDER BY created_at
 DESC;
+
+-- name: InsertNewClasswork :one
+INSERT INTO "class_work" (
+  id, name, user_id, class_id
+) VALUES (
+  $1, $2, $3, $4, $5
+) RETURNING *;
+
+-- name: UpdateAClassworkMark :exec
+UPDATE "class_work"
+SET mark = $1
+WHERE id = $1, user_id = $2, class_id = $3;
+
+-- name: DeleteClassworkFromClass :exec
+DELETE FROM "class_work"
+WHERE id = $1, user_id = $2, class_id = $3;
