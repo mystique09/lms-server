809864d6dfb85aad1d2f05dc71640ef1dd8f3388
mystique09 <knightkrypto7@gmail.com>
2022-07-15 15:36:00 +0800

    feat: remove comments

--- routes/auth_route.go
+++ routes/auth_route.go
@@ -19,23 +19,19 @@ func (rt *Route) Login(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", err.Error()))
 	}
 
-	// check if payloaf is not empty
 	if payload.Username == "" || payload.Password == "" {
 		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", "Username and password are required"))
 	}
 
-	// check if user exists
 	user, err := rt.DB.GetUserByUsername(rt.CTX, payload.Username)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", "User doesn't exist."))
 	}
 
-	// check if password is correct
 	if payload.Password != user.Password {
 		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", "Password mismatch"))
 	}
 
-	// create jwt token
 	token, err := utils.NewJwtToken(utils.NewJwtClaims(utils.NewJwtPayload(user.Username, user.Email, string(user.UserRole))), rt.Cfg.JWT_SECRET_KEY)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", err.Error()))
