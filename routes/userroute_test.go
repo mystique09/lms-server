6954370a95a11d626f4514f20b76a5b9be76aa62
mystique09 <knightkrypto7@gmail.com>
2022-07-16 23:32:57 +0800

    test: add login test

--- routes/userroute_test.go
+++ routes/userroute_test.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"server/utils"
 	"strings"
 	"testing"
@@ -13,6 +14,7 @@ import (
 
 var userRequestJson string = `{"username":"mystique09","password":"testpassword","email":"testemail@gmail.com"}`
 var testUserId string
+var token string
 
 func TestGetUsersRoute(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/api/v1/users", nil)
@@ -53,6 +55,26 @@ func TestCreateUserRoute(t *testing.T) {
 	}
 }
 
+func TestLoginMystique09(t *testing.T) {
+	var payload string = `{"username":"mystique09","password":"testpassword"}`
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(payload))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+
+	if assert.NoError(t, testRoute.loginRoute(ctx)) {
+		res := utils.Response{}
+		if err := utils.GetJson(rec.Body, &res); err != nil {
+			t.Fail()
+		}
+		success_resp := res.Data.(map[string]interface{})
+		assert.Equal(t, success_resp["message"], "Logged in.")
+		assert.NotEmpty(t, success_resp["access_token"])
+		assert.NotEmpty(t, success_resp["refresh_token"])
+		token = success_resp["access_token"].(string)
+	}
+}
+
 func TestGetUserById(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	rec := httptest.NewRecorder()
@@ -72,8 +94,35 @@ func TestGetUserById(t *testing.T) {
 	}
 }
 
+func TestUpdateUsernameById(t *testing.T) {
+	var payload string = `{"username":"updated_username"}`
+
+	q := make(url.Values)
+	q.Set("field", "username")
+
+	req := httptest.NewRequest(http.MethodPut, "/?"+q.Encode(), strings.NewReader(payload))
+	req.Header.Set("authorization", "Bearer "+token)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	ctx.SetPath("/api/v1/users/:id")
+	ctx.SetParamNames("id")
+	ctx.SetParamValues(testUserId)
+
+	if assert.NoError(t, testRoute.updateUser(ctx)) {
+		res := utils.Response{}
+		if err := utils.GetJson(rec.Body, &res); err != nil {
+			t.Fail()
+		}
+		t.Log(res)
+		updated_user := res.Data.(map[string]interface{})
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.NotEqual(t, "mystique09", updated_user["username"])
+	}
+}
+
 func TestDeleteUserById(t *testing.T) {
 	req := httptest.NewRequest(http.MethodDelete, "/", nil)
+	req.Header.Set("authorization", "Bearer "+token)
 	rec := httptest.NewRecorder()
 	ctx := e.NewContext(req, rec)
 	ctx.SetPath("/api/v1/users/:id")
@@ -85,7 +134,8 @@ func TestDeleteUserById(t *testing.T) {
 		if err := utils.GetJson(rec.Body, &res); err != nil {
 			t.Fail()
 		}
+		deleted_user := res.Data.(map[string]interface{})
 		assert.Equal(t, http.StatusOK, rec.Code)
-		assert.Equal(t, testUserId, res.Data)
+		assert.Equal(t, testUserId, deleted_user["id"])
 	}
 }
