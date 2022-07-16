cb98178de37a71ec53436281de7c3fa11cff19ed
mystique09 <knightkrypto7@gmail.com>
2022-07-16 13:17:13 +0800

    feat: used request context

--- routes/index_route.go
+++ routes/index_route.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
-	"context"
+	"log"
+	"net/http"
 	"server/config"
 	database "server/database/sqlc"
+	"server/utils"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 )
 
 type (
@@ -12,7 +17,6 @@ type (
 	*/
 	Route struct {
 		DB  *database.Queries
-		CTX context.Context
 		Cfg config.Config
 	}
 
@@ -24,3 +28,50 @@ type (
 		Body   string
 	}
 )
+
+var rt Route
+
+func Setup() Route {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config := config.Init()
+
+	conn := utils.SetupDB(config.DATABASE_URL)
+	db := database.New(conn)
+
+	return Route{
+		DB:  db,
+		Cfg: config,
+	}
+}
+
+func (rt *Route) IndexHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Welcome! This is my backend API for my Class Management System personal project.")
+}
+
+func Launch() {
+	rt = Setup()
+
+	e := echo.New()
+	e.Use(LoggerMiddleware())
+	e.Use(RateLimitMiddleware(20))
+	e.Use(CorsMiddleware(rt.Cfg))
+
+	e.GET("/api/v1", rt.IndexHandler)
+	e.POST("/api/v1/signup", rt.CreateUser)
+	e.POST("/api/v1/login", rt.Login)
+
+	user_route := e.Group("/api/v1/users", JwtAuthMiddleware(rt.Cfg))
+	{
+		user_route.GET("", rt.GetUsers)
+		user_route.GET("/:id", rt.GetUser)
+		user_route.PUT("/:id", rt.UpdateUser)
+		user_route.DELETE("/:id", rt.DeleteUser)
+	}
+
+	e.Logger.Fatal(e.Start(rt.Cfg.PORT))
+}
