7429b44be200b724abdcfd1e458f6ce461684276
mystique09 <knightkrypto7@gmail.com>
2022-07-16 13:17:25 +0800

    add: index route test

--- routes/index_test.go
+++ routes/index_test.go
@@ -1,1 +1,22 @@
 package routes
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexRoute(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+
+	if assert.NoError(t, rt.IndexHandler(ctx)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Welcome! This is my backend API for my Class Management System personal project.", rec.Body.String())
+	}
+}
