44245ebcdcb0440057ed51d8c1f0e9269be1b5f9
mystique09 <knightkrypto7@gmail.com>
2022-07-16 17:39:48 +0800

    feat: removed status

--- routes/auth_route.go
+++ routes/auth_route.go
@@ -17,26 +17,26 @@ func (rt *Route) loginRoute(c echo.Context) error {
 	var payload AuthRequest
 
 	if err := c.Bind(&payload); err != nil {
-		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", err.Error()))
+		return c.JSON(http.StatusBadRequest, utils.NewResponse("", err.Error()))
 	}
 
 	if payload.Username == "" || payload.Password == "" {
-		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", "Username and password are required"))
+		return c.JSON(http.StatusBadRequest, utils.NewResponse("", "Username and password are required"))
 	}
 
 	user, err := rt.DB.GetUserByUsername(c.Request().Context(), payload.Username)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", "User doesn't exist."))
+		return c.JSON(http.StatusBadRequest, utils.NewResponse("", "User doesn't exist."))
 	}
 
 	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password)) != nil {
-		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", "Password mismatch"))
+		return c.JSON(http.StatusBadRequest, utils.NewResponse("", "Password mismatch"))
 	}
 
 	token, err := utils.NewJwtToken(utils.NewJwtClaims(utils.NewJwtPayload(user.Username, user.Email, string(user.UserRole))), rt.Cfg.JWT_SECRET_KEY)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, utils.NewResponse(0, "", err.Error()))
+		return c.JSON(http.StatusBadRequest, utils.NewResponse("", err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, utils.NewResponse(1, token, ""))
+	return c.JSON(http.StatusOK, utils.NewResponse(token, ""))
 }
