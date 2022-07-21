7579c125eb4df2632f6d340990f29e7411e57c72
mystique09 <knightkrypto7@gmail.com>
2022-07-21 16:41:50 +0800

    feat: unit test for update and delete, test goes slow bruhh

--- database/sqlc/class_test.go
+++ database/sqlc/class_test.go
@@ -9,6 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var class_id uuid.UUID
+var class_invcode uuid.UUID
+
 func TestCreateOneNewClassroom(t *testing.T) {
 	uid := uuid.New()
 	user, err := testQueries.CreateUser(context.Background(), CreateUserParams{
@@ -48,6 +51,8 @@ func TestCreateOneNewClassroom(t *testing.T) {
 	}
 
 	user_id = uid
+	class_id = classroom.ID
+	class_invcode = classroom.InviteCode
 }
 
 func TestCreateNewMoreClassrooms(t *testing.T) {
@@ -98,5 +103,43 @@ func TestCreateNewMoreClassrooms(t *testing.T) {
 
 		}
 	}
+	//testQueries.DeleteUser(context.Background(), user_id)
+}
+
+func TestUpdateOneClassroom(t *testing.T) {
+	args := UpdateClassParams{
+		ID:         class_id,
+		Name:       random.New().String(15, charsets),
+		Subject:    random.New().String(12, charsets),
+		Section:    random.New().String(8, charsets),
+		Room:       random.New().String(5, charsets),
+		InviteCode: uuid.New(),
+	}
+
+	updated, err := testQueries.UpdateClass(context.Background(), args)
+
+	if assert.NoError(t, err) {
+		assert.NotEqual(t, updated.InviteCode, class_invcode)
+		assert.Len(t, updated.Name, 15)
+		assert.Len(t, updated.Subject, 12)
+		assert.Len(t, updated.Section, 8)
+		assert.Len(t, updated.Room, 5)
+	}
+	//testQueries.DeleteUser(context.Background(), user_id)
+}
+
+func TestDeleteClassroom(t *testing.T) {
+	deleted, err := testQueries.DeleteClass(context.Background(), class_id)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, deleted.ID, class_id)
+	}
+
+	class, err := testQueries.GetClass(context.Background(), class_id)
+
+	if assert.Error(t, err) {
+		assert.Equal(t, class.ID, uuid.Nil)
+	}
+
 	testQueries.DeleteUser(context.Background(), user_id)
 }
