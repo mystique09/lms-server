8ee4eb970cfa5f1aac61b976eeb8a8e8de868d59
mystique09 <knightkrypto7@gmail.com>
2022-07-22 22:29:41 +0800

    feat: new queries for followers

--- database/queries/user_follow.sql
+++ database/queries/user_follow.sql
@@ -1,5 +1,17 @@
--- name: GetAllFollowers :many
+-- name: GetOneFollower :one
 SELECT *
+FROM user_follows
+WHERE follower = $1 AND following = $2
+LIMIT 1;
+
+-- name: GetFollowerById :one
+SELECT id, follower AS user_id, created_at, updated_at
+FROM user_follows
+WHERE id = $1
+LIMIT 1;
+
+-- name: GetAllFollowers :many
+SELECT id, follower AS user_id, created_at, updated_at
 FROM user_follows
 WHERE following = $1
 ORDER BY created_at
@@ -7,14 +19,14 @@ LIMIT 10
 OFFSET $2;
 
 -- name: GetAllFollowing :many
-SELECT *
+SELECT id, following AS user_id, created_at, updated_at
 FROM user_follows
 WHERE follower = $1
 ORDER BY created_at
 LIMIT 10
 OFFSET $2;
 
--- namw: FollowUser :one
+-- name: FollowUser :one
 INSERT INTO user_follows
 (id, follower, following)
 VALUES ($1, $2, $3)
