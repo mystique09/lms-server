3211b6d6f9244af021be66d7344b59318071eff6
mystique09 <knightkrypto7@gmail.com>
2022-07-23 02:58:43 +0800

    feat: add initial scripts

---
+++ web/web.go
@@ -0,0 +1,18 @@
+package web
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+)
+
+//go:embed all:dist/*
+var BuildFs embed.FS
+
+func BuildWebFS() fs.FS {
+	build, err := fs.Sub(BuildFs, "dist")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return build
+}
