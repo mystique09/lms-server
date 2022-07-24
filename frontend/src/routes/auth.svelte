c093502bcb7605d3f0d6abcd5d8eba17f4691e3c
mystique09 <knightkrypto7@gmail.com>
2022-07-24 22:27:22 +0800

    feat: removed the tsc command

---
+++ frontend/src/routes/auth.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	export async function load({ session }) {
+		if (!session) {
+			return {
+				status: 301,
+				redirect: '/'
+			};
+		}
+	}
+</script>
+
+<div>Dashboard</div>
