35a5ee72e1235ee460e40af612e139773b53ddda
mystique09 <knightkrypto7@gmail.com>
2022-07-29 14:03:36 +0800

    add: static variable for errors

--- routes/errors.go
+++ routes/errors.go
@@ -1,1 +1,9 @@
 package routes
+
+import "server/utils"
+
+var LOGIN_FAILED utils.Response = utils.NewResponse(nil, "Incorrect username or password.")
+var USER_NOTFOUND utils.Response = utils.NewResponse(nil, "User doesn't exist.")
+var UNAUTHORIZED utils.Response = utils.NewResponse(nil, "You are not authorized to perform this action.")
+var CLASSROOM_NOTFOUND utils.Response = utils.NewResponse(nil, "Classroom doesn't exist.")
+var POST_NOTFOUND utils.Response = utils.NewResponse(nil, "Post doesn't exist.")
