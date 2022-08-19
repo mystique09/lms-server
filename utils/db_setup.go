0f95e1234f5a67c9c4ffd73d15c50c2dd2306a01
mystique09 <knightkrypto7@gmail.com>
2022-08-20 00:14:24 +0800

    feat: remove migrate

--- utils/db_setup.go
+++ utils/db_setup.go
@@ -2,9 +2,6 @@ package utils
 
 import (
 	"database/sql"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 	"log"
 )
@@ -16,19 +13,6 @@ func SetupDB(DATABASE_URL string) *sql.DB {
 		log.Fatal(err)
 	}
 	//defer db.Close()
-
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://database/migrations",
-		"postgres", driver)
-
-	if err != nil {
-		log.Fatalf("unable to migrate: %v", err.Error())
-	}
-
-	log.Println("Starting database migration")
-	m.Up()
-	log.Println("Migration success")
 
 	return db
 }
