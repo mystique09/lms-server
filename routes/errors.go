d9be4a62b66f93dc2762c55b8587f793bda20418
mystique09 <knightkrypto7@gmail.com>
2022-11-18 01:41:36 +0800

    fix: use newResponse method

--- routes/errors.go
+++ routes/errors.go
@@ -1,30 +1,26 @@
 package routes
 
-import (
-	"server/utils"
-)
-
 // auth errors
-var LOGIN_FAILED utils.Response = utils.NewResponse(nil, "Incorrect username or password.")
-var USER_NOTFOUND utils.Response = utils.NewResponse(nil, "User doesn't exist.")
-var MISSING_FIELDS utils.Response = utils.NewResponse(nil, "Missing required fields.")
-var USER_ALREADY_EXIST utils.Response = utils.NewResponse(nil, "User already exist.")
+var LOGIN_FAILED Response = newResponse(nil, "Incorrect username or password.")
+var USER_NOTFOUND Response = newResponse(nil, "User doesn't exist.")
+var MISSING_FIELDS Response = newResponse(nil, "Missing required fields.")
+var USER_ALREADY_EXIST Response = newResponse(nil, "User already exist.")
 
 // role validation errors
-var UNAUTHORIZED utils.Response = utils.NewResponse(nil, "You are not authorized to perform this action.")
-var NOT_A_MEMBER utils.Response = utils.NewResponse(nil, "You are not a member of this classroom.")
+var UNAUTHORIZED Response = newResponse(nil, "You are not authorized to perform this action.")
+var NOT_A_MEMBER Response = newResponse(nil, "You are not a member of this classroom.")
 
 // classroom errors
-var CLASSROOM_NOTFOUND utils.Response = utils.NewResponse(nil, "Classroom doesn't exist.")
+var CLASSROOM_NOTFOUND Response = newResponse(nil, "Classroom doesn't exist.")
 
 // classworks errors
-var CLASSWORK_NOTFOUND utils.Response = utils.NewResponse(nil, "Classwork doesn't exist.")
+var CLASSWORK_NOTFOUND Response = newResponse(nil, "Classwork doesn't exist.")
 
 // post errors
-var POST_NOTFOUND utils.Response = utils.NewResponse(nil, "Post doesn't exist.")
+var POST_NOTFOUND Response = newResponse(nil, "Post doesn't exist.")
 
 // field errors
-var UNKNOWN_FIELD utils.Response = utils.NewResponse(nil, "Unknown field in query params.")
-var MISSING_ID_FIELD utils.Response = utils.NewResponse(nil, "Missing id field in path parameter.")
-var EMPTY_QUERY_PARAM utils.Response = utils.NewResponse(nil, "Empty query parameter.")
-var NEGATIVE_OFFSET utils.Response = utils.NewResponse(nil, "Offset must not be negative.")
+var UNKNOWN_FIELD Response = newResponse(nil, "Unknown field in query params.")
+var MISSING_ID_FIELD Response = newResponse(nil, "Missing id field in path parameter.")
+var EMPTY_QUERY_PARAM Response = newResponse(nil, "Empty query parameter.")
+var NEGATIVE_OFFSET Response = newResponse(nil, "Offset must not be negative.")
