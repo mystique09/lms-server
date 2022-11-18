70174e3f8dbdddfe373c62b593cd62bca38d2b76
mystique09 <knightkrypto7@gmail.com>
2022-11-18 15:21:27 +0800

    add: config using viper

---
+++ utils/config.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBUrl               string `mapstructure:"DB_URL"`
+	Host                string `mapstructure:"HOST"`
+	JwtSecretKey        string `mapstructure:"JWT_SECRET"`
+	JwtRefreshSecretKey string `mapstructure:"JWT_REFRESH"`
+	FrontendUrl         string `mapstructure:"FRONTEND_URL"`
+	CldUrl              string `mapstructure:"CLD_URL"`
+}
+
+func LoadConfig(path string) (Config, error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	err = viper.Unmarshal(&config)
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	log.Println("Loaded config file")
+	return config, nil
+}
