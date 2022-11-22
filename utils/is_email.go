b5e89e9e1898d3339e407656bce26358d84d85dc
mystique09 <knightkrypto7@gmail.com>
2022-11-22 13:23:44 +0800

    chore: remove token

--- utils/is_email.go
+++ utils/is_email.go
@@ -3,7 +3,6 @@ package utils
 import "regexp"
 
 func IsEmail(email string) bool {
-	// validate email using regex
 	re := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	return re.MatchString(email)
 }
