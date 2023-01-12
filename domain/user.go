a3104d2ae3dd9b079d071c635137cdc10de94306
mystique09 <knightkrypto7@gmail.com>
2023-01-13 01:08:06 +0800

    add: user domain

--- domain/user.go
+++ domain/user.go
@@ -1,4 +1,21 @@
 package domain
 
-type User struct {
+import (
+	"server/database/postgresql"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type User = postgresql.User
+
+type UserRepository interface {
+	Create(c echo.Context, u *postgresql.CreateUserParams) error
+	Fetch(c echo.Context, offsent int32) ([]User, error)
+	GetByID(c echo.Context, id uuid.UUID) (User, error)
+	GetByUsername(c echo.Context, email string) (User, error)
+	UpdateUsername(c echo.Context, u *postgresql.UpdateUsernameParams) error
+	UpdateEmail(c echo.Context, u *postgresql.UpdateUserEmailParams) error
+	UpdatePassword(c echo.Context, u *postgresql.UpdateUserPasswordParams) error
+	Delete(c echo.Context, id uuid.UUID) error
 }
