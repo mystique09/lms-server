c36da01d2e64a0977c21ed5b5e8d1af589bab9d0
mystique09 <knightkrypto7@gmail.com>
2023-01-13 01:10:28 +0800

    add: paseto token maker

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -4,10 +4,14 @@ import (
 	"database/sql"
 	"log"
 	"server/database/store"
+	"server/internal/tokenutil"
+
+	"github.com/labstack/echo/v4"
 )
 
 type Application struct {
 	Env              Env
+	TokenMaker       tokenutil.Maker
 	Store            store.Store
 	PostgresqlClient *sql.DB
 }
@@ -19,10 +23,20 @@ func App() Application {
 		log.Fatal(err)
 	}
 
+	tokenMaker, err := tokenutil.NewPasetoMaker(env.PasetoSymmetricKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app.Env = env
+	app.TokenMaker = tokenMaker
 	app.PostgresqlClient = NewPostgresqlClient(&env)
 	app.Store = store.NewStore(app.PostgresqlClient)
 	return *app
+}
+
+func (app *Application) Launch(e *echo.Echo) {
+	log.Fatal(e.Start(app.Env.Host + ":" + app.Env.Port))
 }
 
 func (app *Application) CloseDBConnection() {
