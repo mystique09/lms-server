72dcf0cc1d6c3454da310772a8e1308fb5db0d60
mystique09 <knightkrypto7@gmail.com>
2023-01-13 01:10:09 +0800

    feat: move Launch method to Application struct

--- cmd/main.go
+++ cmd/main.go
@@ -9,12 +9,11 @@ import (
 
 func main() {
 	app := bootstrap.App()
-	env := app.Env
 	defer app.CloseDBConnection()
 
 	e := echo.New()
 	routeV1 := e.Group("/api/v1")
 
-	route.Setup(&env, app.Store, routeV1)
-	route.Launch(&env, e)
+	route.Setup(&app, app.Store, routeV1)
+	app.Launch(e)
 }
