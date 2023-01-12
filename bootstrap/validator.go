33500178c29931cad8873c48f3040ad56de0e472
mystique09 <knightkrypto7@gmail.com>
2023-01-13 01:18:40 +0800

    add: validator

---
+++ bootstrap/validator.go
@@ -0,0 +1,23 @@
+package bootstrap
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+)
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func NewValidator() *CustomValidator {
+	return &CustomValidator{validator: validator.New()}
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
