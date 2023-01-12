211e2f2efd9c266ab998b2bda0f8502198c2a866
mystique09 <knightkrypto7@gmail.com>
2023-01-13 01:08:14 +0800

    add: login domain

---
+++ domain/login.go
@@ -0,0 +1,27 @@
+package domain
+
+import (
+	"server/internal/tokenutil"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type LoginRequest struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type LoginResponse struct {
+	User                   User      `json:"user"`
+	AccessToken            string    `json:"access_token"`
+	AccessTokenExpiration  time.Time `json:"access_token_expiration"`
+	RefreshToken           string    `json:"refresh_token"`
+	RefreshTokenExpiration time.Time `json:"refresh_token_expiration"`
+}
+
+type LoginUsecase interface {
+	GetUserByUsername(c echo.Context, username string) (User, error)
+	CreateAccessToken(username string, duration time.Duration) (string, *tokenutil.Payload, error)
+	CreateRefreshToken(username string, duration time.Duration) (string, *tokenutil.Payload, error)
+}
