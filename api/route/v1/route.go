de5383f273167f92343cfc47bf841a5ad814a176
mystique09 <knightkrypto7@gmail.com>
2023-01-12 14:56:44 +0800

    fix: json not serialized

--- api/route/v1/route.go
+++ api/route/v1/route.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"server/bootstrap"
 	"server/database/store"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -15,14 +14,14 @@ func Setup(env *bootstrap.Env, st store.Store, routeV1 *echo.Group) {
 	// TODO!
 	// remove this one
 	publicRouterV1.GET("/health", func(c echo.Context) error {
-		return c.JSON(200, strings.NewReader(`{"health":"100", "status": "good"}`))
+		return c.JSON(200, `{"health":"100", "status": "good"}`)
 	})
 
 	protectedRouterV1 := routeV1.Group("")
 	// TODO!
 	// remove this one
 	protectedRouterV1.GET("/protected", func(c echo.Context) error {
-		return c.JSON(200, strings.NewReader(`{"message": "protected"}`))
+		return c.JSON(200, `{"message": "protected"}`)
 	})
 }
 
