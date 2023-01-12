092c11b75d000c2ab51c5a2fdbd5d3d606c17be3
mystique09 <knightkrypto7@gmail.com>
2023-01-13 01:08:38 +0800

    add: login usecase

--- usecase/login_usecase.go
+++ usecase/login_usecase.go
@@ -1,1 +1,33 @@
 package usecase
+
+import (
+	"server/domain"
+	"server/internal/tokenutil"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type loginUsecase struct {
+	userRepository domain.UserRepository
+	tokenMaker     tokenutil.Maker
+}
+
+func NewLoginUsecase(userRepository domain.UserRepository, tokenMaker tokenutil.Maker) domain.LoginUsecase {
+	return &loginUsecase{
+		userRepository: userRepository,
+		tokenMaker:     tokenMaker,
+	}
+}
+
+func (lu *loginUsecase) GetUserByUsername(c echo.Context, username string) (domain.User, error) {
+	return lu.userRepository.GetByUsername(c, username)
+}
+
+func (lu *loginUsecase) CreateAccessToken(username string, duration time.Duration) (string, *tokenutil.Payload, error) {
+	return lu.tokenMaker.CreateToken(username, duration)
+}
+
+func (lu *loginUsecase) CreateRefreshToken(username string, duration time.Duration) (string, *tokenutil.Payload, error) {
+	return lu.tokenMaker.CreateToken(username, duration)
+}
