2e68aed6255cabc2b13fa67fe6edcaa5121a1a48
mystique09 <knightkrypto7@gmail.com>
2023-01-12 14:27:39 +0800

    add: tokenutil

---
+++ internal/tokenutil/maker.go
@@ -0,0 +1,9 @@
+package tokenutil
+
+import "time"
+
+type Maker interface {
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
+
+	VerifyToken(token string) (*Payload, error)
+}
