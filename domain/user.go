defb6ece69cf9326ecf067d8827d101750766028
mystique09 <knightkrypto7@gmail.com>
2023-01-16 02:16:27 +0800

    feat: use contex.Context

--- domain/user.go
+++ domain/user.go
@@ -1,21 +1,21 @@
 package domain
 
 import (
+	"context"
 	"server/database/postgresql"
 
 	"github.com/google/uuid"
-	"github.com/labstack/echo/v4"
 )
 
 type User = postgresql.User
 
 type UserRepository interface {
-	Create(c echo.Context, u *postgresql.CreateUserParams) error
-	Fetch(c echo.Context, offsent int32) ([]User, error)
-	GetByID(c echo.Context, id uuid.UUID) (User, error)
-	GetByUsername(c echo.Context, email string) (User, error)
-	UpdateUsername(c echo.Context, u *postgresql.UpdateUsernameParams) error
-	UpdateEmail(c echo.Context, u *postgresql.UpdateUserEmailParams) error
-	UpdatePassword(c echo.Context, u *postgresql.UpdateUserPasswordParams) error
-	Delete(c echo.Context, id uuid.UUID) error
+	Create(c context.Context, u *postgresql.CreateUserParams) error
+	Fetch(c context.Context, offsent int32) ([]User, error)
+	GetByID(c context.Context, id uuid.UUID) (User, error)
+	GetByUsername(c context.Context, email string) (User, error)
+	UpdateUsername(c context.Context, u *postgresql.UpdateUsernameParams) error
+	UpdateEmail(c context.Context, u *postgresql.UpdateUserEmailParams) error
+	UpdatePassword(c context.Context, u *postgresql.UpdateUserPasswordParams) error
+	Delete(c context.Context, id uuid.UUID) error
 }
