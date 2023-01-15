14de9d3a87cd7ef39e4efd290bd2b28d597cdf1b
mystique09 <knightkrypto7@gmail.com>
2023-01-16 02:16:35 +0800

    feat: use context.Context

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -5,8 +5,8 @@ import (
 	"server/database/store"
 	"server/domain"
 
+	"context"
 	"github.com/google/uuid"
-	"github.com/labstack/echo/v4"
 )
 
 type userRepository struct {
@@ -19,42 +19,42 @@ func NewUserRepository(st store.Store) domain.UserRepository {
 	}
 }
 
-func (ur *userRepository) Create(c echo.Context, u *postgresql.CreateUserParams) error {
-	_, err := ur.store.CreateUser(c.Request().Context(), *u)
+func (ur *userRepository) Create(c context.Context, u *postgresql.CreateUserParams) error {
+	_, err := ur.store.CreateUser(c, *u)
 	return err
 }
 
-func (ur *userRepository) Fetch(c echo.Context, offset int32) ([]domain.User, error) {
-	users, err := ur.store.GetUsers(c.Request().Context(), offset*10)
+func (ur *userRepository) Fetch(c context.Context, offset int32) ([]domain.User, error) {
+	users, err := ur.store.GetUsers(c, offset*10)
 	return users, err
 }
 
-func (ur *userRepository) GetByID(c echo.Context, id uuid.UUID) (domain.User, error) {
-	user, err := ur.store.GetUser(c.Request().Context(), id)
+func (ur *userRepository) GetByID(c context.Context, id uuid.UUID) (domain.User, error) {
+	user, err := ur.store.GetUser(c, id)
 	return user, err
 }
 
-func (ur *userRepository) GetByUsername(c echo.Context, username string) (domain.User, error) {
-	user, err := ur.store.GetUserByUsername(c.Request().Context(), username)
+func (ur *userRepository) GetByUsername(c context.Context, username string) (domain.User, error) {
+	user, err := ur.store.GetUserByUsername(c, username)
 	return user, err
 }
 
-func (ur *userRepository) UpdateUsername(c echo.Context, u *postgresql.UpdateUsernameParams) error {
-	_, err := ur.store.UpdateUsername(c.Request().Context(), *u)
+func (ur *userRepository) UpdateUsername(c context.Context, u *postgresql.UpdateUsernameParams) error {
+	_, err := ur.store.UpdateUsername(c, *u)
 	return err
 }
 
-func (ur *userRepository) UpdateEmail(c echo.Context, u *postgresql.UpdateUserEmailParams) error {
-	_, err := ur.store.UpdateUserEmail(c.Request().Context(), *u)
+func (ur *userRepository) UpdateEmail(c context.Context, u *postgresql.UpdateUserEmailParams) error {
+	_, err := ur.store.UpdateUserEmail(c, *u)
 	return err
 }
 
-func (ur *userRepository) UpdatePassword(c echo.Context, u *postgresql.UpdateUserPasswordParams) error {
-	_, err := ur.store.UpdateUserPassword(c.Request().Context(), *u)
+func (ur *userRepository) UpdatePassword(c context.Context, u *postgresql.UpdateUserPasswordParams) error {
+	_, err := ur.store.UpdateUserPassword(c, *u)
 	return err
 }
 
-func (ur *userRepository) Delete(c echo.Context, id uuid.UUID) error {
-	_, err := ur.store.DeleteUser(c.Request().Context(), id)
+func (ur *userRepository) Delete(c context.Context, id uuid.UUID) error {
+	_, err := ur.store.DeleteUser(c, id)
 	return err
 }
