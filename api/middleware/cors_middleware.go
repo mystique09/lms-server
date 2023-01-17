7d248ce0da8fcc8ac1e20cdccf7649fd38233e78
mystique09 <knightkrypto7@gmail.com>
2023-01-17 11:09:10 +0800

    chore(env): rename cfg to env

--- api/middleware/cors_middleware.go
+++ api/middleware/cors_middleware.go
@@ -7,10 +7,10 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func CorsMiddleware(cfg *bootstrap.Env) echo.MiddlewareFunc {
+func CorsMiddleware(env *bootstrap.Env) echo.MiddlewareFunc {
 	return middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{
-			cfg.FrontendUrl,
+			env.FrontendUrl,
 		},
 		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders: []string{
