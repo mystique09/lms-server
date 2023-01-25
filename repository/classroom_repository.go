4526e2e0617590fb331c7700491cded5b3d73231
mystique09 <knightkrypto7@gmail.com>
2023-01-25 11:36:34 +0800

    feat: classroom repository

--- repository/classroom_repository.go
+++ repository/classroom_repository.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"server/database/postgresql"
 	"server/database/store"
-	"server/domain"
+	"server/domain/classroom"
 
 	"github.com/google/uuid"
 )
@@ -19,8 +19,8 @@ func NewClassroomRepository(st store.Store) domain.ClassroomRepository {
 	}
 }
 
-func (cr *classroomRepository) Create(c context.Context, crp *postgresql.CreateClassParams) error {
-	_, err := cr.store.CreateClass(c, *crp)
+func (cr *classroomRepository) Create(c context.Context, cl *postgresql.CreateClassParams) error {
+	_, err := cr.store.CreateClass(c, *cl)
 	return err
 }
 
@@ -44,33 +44,33 @@ func (cr *classroomRepository) GetClasroomByUser(c context.Context, opts postgre
 	return classrooms, err
 }
 
-func (cr *classroomRepository) UpdateClassroomName(c context.Context, u *postgresql.UpdateClassroomNameParams) error {
-	_, err := cr.store.UpdateClassroomName(c, *u)
+func (cr *classroomRepository) UpdateClassroomName(c context.Context, cl *postgresql.UpdateClassroomNameParams) error {
+	_, err := cr.store.UpdateClassroomName(c, *cl)
 	return err
 }
 
-func (cr *classroomRepository) UpdateClassroomDescription(c context.Context, u *postgresql.UpdateClassroomDescriptionParams) error {
-	_, err := cr.store.UpdateClassroomDescription(c, *u)
+func (cr *classroomRepository) UpdateClassroomDescription(c context.Context, cl *postgresql.UpdateClassroomDescriptionParams) error {
+	_, err := cr.store.UpdateClassroomDescription(c, *cl)
 	return err
 }
 
-func (cr *classroomRepository) UpdateClassroomSection(c context.Context, u *postgresql.UpdateClassroomSectionParams) error {
-	_, err := cr.store.UpdateClassroomSection(c, *u)
+func (cr *classroomRepository) UpdateClassroomSection(c context.Context, cl *postgresql.UpdateClassroomSectionParams) error {
+	_, err := cr.store.UpdateClassroomSection(c, *cl)
 	return err
 }
 
-func (cr *classroomRepository) UpdateClassroomRoom(c context.Context, u *postgresql.UpdateClassroomRoomParams) error {
-	_, err := cr.store.UpdateClassroomRoom(c, *u)
+func (cr *classroomRepository) UpdateClassroomRoom(c context.Context, cl *postgresql.UpdateClassroomRoomParams) error {
+	_, err := cr.store.UpdateClassroomRoom(c, *cl)
 	return err
 }
 
-func (cr *classroomRepository) UpdateClassroomSubject(c context.Context, u *postgresql.UpdateClassroomSubjectParams) error {
-	_, err := cr.store.UpdateClassroomSubject(c, *u)
+func (cr *classroomRepository) UpdateClassroomSubject(c context.Context, cl *postgresql.UpdateClassroomSubjectParams) error {
+	_, err := cr.store.UpdateClassroomSubject(c, *cl)
 	return err
 }
 
-func (cr *classroomRepository) UpdateClassroomInviteCode(c context.Context, u *postgresql.UpdateClassroomInviteCodeParams) error {
-	_, err := cr.store.UpdateClassroomInviteCode(c, *u)
+func (cr *classroomRepository) UpdateClassroomInviteCode(c context.Context, cl *postgresql.UpdateClassroomInviteCodeParams) error {
+	_, err := cr.store.UpdateClassroomInviteCode(c, *cl)
 	return err
 }
 
