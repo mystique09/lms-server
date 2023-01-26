4d9cd24262a0d8e89a6420a78c36bec7ed755ed2
mystique09 <knightkrypto7@gmail.com>
2023-01-26 10:43:02 +0800

    add: classroom controller

--- api/controller/classroom_controller.go
+++ api/controller/classroom_controller.go
@@ -1,1 +1,34 @@
 package controller
+
+import (
+	domain "server/domain/classroom"
+
+	"github.com/labstack/echo/v4"
+)
+
+type ClassroomController struct {
+	GetUsecase    domain.GetClassroomUsecase
+	CreateUsecase domain.CreateClassroomUsecase
+	UpdateUsecase domain.UpdateClassroomUsecase
+	DeleteUsecase domain.DeleteClassroomUsecase
+}
+
+func (clc *ClassroomController) GetClassrooms(c echo.Context) error {
+	return c.String(200, "[GET] TODO!")
+}
+
+func (clc *ClassroomController) GetClassroom(c echo.Context) error {
+	return c.String(200, "[GET:all] TODO!")
+}
+
+func (clc *ClassroomController) CreateClassroom(c echo.Context) error {
+	return c.String(200, "[POST] TODO!")
+}
+
+func (clc *ClassroomController) UpdateClassroom(c echo.Context) error {
+	return c.String(200, "[PUT] TODO!")
+}
+
+func (clc *ClassroomController) DeleteClassroom(c echo.Context) error {
+	return c.String(200, "[DELETE] TODO!")
+}
