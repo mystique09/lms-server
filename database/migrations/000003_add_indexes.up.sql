c3a10559c76a3f61bdad12b1d562e6951caef9e5
mystique09 <knightkrypto7@gmail.com>
2023-01-27 10:45:37 +0800

    chore: indexes

--- database/migrations/000003_add_indexes.up.sql
+++ database/migrations/000003_add_indexes.up.sql
@@ -1,21 +1,5 @@
-CREATE INDEX ON "users" ("id");
-
-CREATE INDEX ON "users" ("username");
-
-CREATE INDEX ON "users" ("email");
-
-CREATE INDEX ON "class_works" ("user_id");
-
-CREATE INDEX ON "class_works" ("class_id");
-
-CREATE INDEX ON "classroom_members" ("user_id");
-
-CREATE INDEX ON "classroom_members" ("class_id");
-
-CREATE INDEX ON "post_likes" ("user_id");
-
-CREATE INDEX ON "post_likes" ("post_id");
-
-CREATE INDEX ON "comment_likes" ("user_id");
-
-CREATE INDEX ON "comment_likes" ("comment_id");
+CREATE UNIQUE INDEX users_index ON "users" ("id", "username", "email");
+CREATE UNIQUE INDEX class_works_index ON "class_works" ("user_id", "class_id");
+CREATE UNIQUE INDEX classroom_members_index ON "classroom_members" ("user_id", "class_id");
+CREATE UNIQUE INDEX post_likes_index ON "post_likes" ("user_id", "post_id");
+CREATE UNIQUE INDEX comment_likes_index ON "comment_likes" ("user_id", "comment_id");
