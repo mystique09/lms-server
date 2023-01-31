0e13bcc630a022e4a6e37b99057738c34de14440
mystique09 <knightkrypto7@gmail.com>
2023-01-31 14:54:16 +0800

    chore: format

--- domain/classroom.go
+++ domain/classroom.go
@@ -7,64 +7,70 @@ import (
 	"github.com/google/uuid"
 )
 
-type Classroom = postgresql.Classroom
-type ClassroomMember = postgresql.ClassroomMember
+type (
+	Classroom       = postgresql.Classroom
+	ClassroomMember = postgresql.ClassroomMember
+)
 
-type ClassroomRepository interface {
-	Create(c context.Context, cl *postgresql.CreateClassParams) error
-	Fetch(c context.Context, offsent int32) ([]Classroom, error)
-	GetByID(c context.Context, id uuid.UUID) (Classroom, error)
-	GetByInviteCode(c context.Context, inviteCode uuid.UUID) (uuid.UUID, error)
-	GetClasroomsByUser(c context.Context, opts postgresql.GetAllClassFromUserParams) ([]Classroom, error)
-	GetClassroomMembers(c context.Context, id uuid.UUID) ([]ClassroomMember, error)
-	UpdateClassroomName(c context.Context, cl *postgresql.UpdateClassroomNameParams) error
-	UpdateClassroomDescription(c context.Context, cl *postgresql.UpdateClassroomDescriptionParams) error
-	UpdateClassroomSection(c context.Context, cl *postgresql.UpdateClassroomSectionParams) error
-	UpdateClassroomRoom(c context.Context, cl *postgresql.UpdateClassroomRoomParams) error
-	UpdateClassroomSubject(c context.Context, cl *postgresql.UpdateClassroomSubjectParams) error
-	UpdateClassroomInviteCode(c context.Context, cl *postgresql.UpdateClassroomInviteCodeParams) error
-	Delete(c context.Context, id uuid.UUID) error
-}
+type (
+	CreateClassroomRequest struct {
+		AdminID     uuid.UUID `json:"admin_id" validate:"required"`
+		Name        string    `json:"name" validate:"required,gte=8"`
+		Description string    `json:"description" validate:"required,gte=1"`
+		Section     string    `json:"section" validate:"required,gte=1"`
+		Room        string    `json:"room" validate:"required,gte=1"`
+		Subject     string    `json:"subject" validate:"required,gte=1"`
+	}
 
-type CreateClassroomRequest struct {
-	AdminID     uuid.UUID `json:"admin_id" validate:"required"`
-	Name        string    `json:"name" validate:"required,gte=8"`
-	Description string    `json:"description" validate:"required,gte=1"`
-	Section     string    `json:"section" validate:"required,gte=1"`
-	Room        string    `json:"room" validate:"required,gte=1"`
-	Subject     string    `json:"subject" validate:"required,gte=1"`
-}
+	UpdateClassroomRequest struct {
+		Name        string `json:"name" validate:"gte=0"`
+		Description string `json:"description" validate:"gte=0"`
+		Section     string `json:"section" validate:"gte=0"`
+		Room        string `json:"room" validate:"gte=0"`
+		Subject     string `json:"subject" validate:"gte=0"`
+	}
+)
 
-type UpdateClassroomRequest struct {
-	Name        string `json:"name" validate:"gte=0"`
-	Description string `json:"description" validate:"gte=0"`
-	Section     string `json:"section" validate:"gte=0"`
-	Room        string `json:"room" validate:"gte=0"`
-	Subject     string `json:"subject" validate:"gte=0"`
-}
+type (
+	ClassroomRepository interface {
+		Create(c context.Context, cl *postgresql.CreateClassParams) error
+		Fetch(c context.Context, offsent int32) ([]Classroom, error)
+		GetByID(c context.Context, id uuid.UUID) (Classroom, error)
+		GetByInviteCode(c context.Context, inviteCode uuid.UUID) (uuid.UUID, error)
+		GetClasroomsByUser(c context.Context, opts postgresql.GetAllClassFromUserParams) ([]Classroom, error)
+		GetClassroomMembers(c context.Context, id uuid.UUID) ([]ClassroomMember, error)
+		UpdateClassroomName(c context.Context, cl *postgresql.UpdateClassroomNameParams) error
+		UpdateClassroomDescription(c context.Context, cl *postgresql.UpdateClassroomDescriptionParams) error
+		UpdateClassroomSection(c context.Context, cl *postgresql.UpdateClassroomSectionParams) error
+		UpdateClassroomRoom(c context.Context, cl *postgresql.UpdateClassroomRoomParams) error
+		UpdateClassroomSubject(c context.Context, cl *postgresql.UpdateClassroomSubjectParams) error
+		UpdateClassroomInviteCode(c context.Context, cl *postgresql.UpdateClassroomInviteCodeParams) error
+		Delete(c context.Context, id uuid.UUID) error
+	}
 
-type CreateClassroomUsecase interface {
-	Create(c context.Context, u *postgresql.CreateClassParams) error
-}
+	GetClassroomUsecase interface {
+		GetByID(c context.Context, id uuid.UUID) (Classroom, error)
+		GetByInviteCode(c context.Context, inviteCode uuid.UUID) (uuid.UUID, error)
+		GetClasroomsByUser(c context.Context, opts postgresql.GetAllClassFromUserParams) ([]Classroom, error)
+		GetClassroomMembers(c context.Context, id uuid.UUID) ([]ClassroomMember, error)
+	}
 
-type DeleteClassroomUsecase interface {
-	GetByID(c context.Context, id uuid.UUID) (Classroom, error)
-	Delete(c context.Context, id uuid.UUID) error
-}
+	CreateClassroomUsecase interface {
+		Create(c context.Context, u *postgresql.CreateClassParams) error
+	}
 
-type GetClassroomUsecase interface {
-	GetByID(c context.Context, id uuid.UUID) (Classroom, error)
-	GetByInviteCode(c context.Context, inviteCode uuid.UUID) (uuid.UUID, error)
-	GetClasroomsByUser(c context.Context, opts postgresql.GetAllClassFromUserParams) ([]Classroom, error)
-	GetClassroomMembers(c context.Context, id uuid.UUID) ([]ClassroomMember, error)
-}
+	UpdateClassroomUsecase interface {
+		GetByID(c context.Context, id uuid.UUID) (Classroom, error)
+		UpdateClassroomName(c context.Context, cl *postgresql.UpdateClassroomNameParams) error
+		UpdateClassroomDescription(c context.Context, cl *postgresql.UpdateClassroomDescriptionParams) error
+		UpdateClassroomSection(c context.Context, cl *postgresql.UpdateClassroomSectionParams) error
+		UpdateClassroomRoom(c context.Context, cl *postgresql.UpdateClassroomRoomParams) error
+		UpdateClassroomSubject(c context.Context, cl *postgresql.UpdateClassroomSubjectParams) error
+		UpdateClassroomInviteCode(c context.Context, cl *postgresql.UpdateClassroomInviteCodeParams) error
+	}
 
-type UpdateClassroomUsecase interface {
-	GetByID(c context.Context, id uuid.UUID) (Classroom, error)
-	UpdateClassroomName(c context.Context, cl *postgresql.UpdateClassroomNameParams) error
-	UpdateClassroomDescription(c context.Context, cl *postgresql.UpdateClassroomDescriptionParams) error
-	UpdateClassroomSection(c context.Context, cl *postgresql.UpdateClassroomSectionParams) error
-	UpdateClassroomRoom(c context.Context, cl *postgresql.UpdateClassroomRoomParams) error
-	UpdateClassroomSubject(c context.Context, cl *postgresql.UpdateClassroomSubjectParams) error
-	UpdateClassroomInviteCode(c context.Context, cl *postgresql.UpdateClassroomInviteCodeParams) error
-}
+	DeleteClassroomUsecase interface {
+		GetByID(c context.Context, id uuid.UUID) (Classroom, error)
+		Delete(c context.Context, id uuid.UUID) error
+	}
+)
