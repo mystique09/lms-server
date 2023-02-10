31cf1ae1faf2f68e73135ba554b669a003402420
mystique09 <knightkrypto7@gmail.com>
2023-02-10 12:08:00 +0800

    add: classwork repository

--- repository/classwork_repository.go
+++ repository/classwork_repository.go
@@ -1,42 +1,74 @@
 package repository
 
 import (
+	"bytes"
 	"context"
-	"github.com/google/uuid"
 	"server/bootstrap"
+	"server/database/postgresql"
 	"server/database/store"
 	"server/domain"
 
 	"github.com/cloudinary/cloudinary-go/v2/api/admin"
+	"github.com/google/uuid"
 )
 
 type classworkRepository struct {
-	store    store.Store
-	provider bootstrap.Storage
+	store   store.Store
+	storage bootstrap.Storage
 }
 
-func NewClassworkRepository(s store.Store, p bootstrap.Storage) domain.ClassworkRepository {
+func NewClassworkRepository(s store.Store, storage bootstrap.Storage) domain.ClassworkRepository {
 	return &classworkRepository{
-		store:    s,
-		provider: p,
+		store:   s,
+		storage: storage,
 	}
 }
 
 // TODO!: implement methods
 
-func (cw *classworkRepository) GetOne(c context.Context, id domain.AssetID) (admin.AssetResult, error) {
-	return admin.AssetResult{}, nil
+func (cw *classworkRepository) GetOne(c context.Context, id, user_id uuid.UUID) (domain.Classwork, error) {
+	class_work, err := cw.store.GetClassWork(c, postgresql.GetClassWorkParams{
+		ID:     id,
+		UserID: user_id,
+	})
+	return class_work, err
 }
 
-func (cw *classworkRepository) GetAllClassworksByUserID(c context.Context, id uuid.UUID) (admin.AssetsResult, error) {
-	return admin.AssetsResult{}, nil
+func (cw *classworkRepository) GetAllClassworksByUserID(c context.Context, user_id uuid.UUID, offset int32) ([]domain.Classwork, error) {
+	class_works, err := cw.store.ListSubmittedClassworks(c, postgresql.ListSubmittedClassworksParams{
+		UserID: user_id,
+		Offset: offset,
+	})
+	return class_works, err
 }
-func (cw *classworkRepository) GetAllClassworksByClassID(c context.Context, id uuid.UUID) (admin.AssetsResult, error) {
-	return admin.AssetsResult{}, nil
+
+func (cw *classworkRepository) GetAllClassworksByClassID(c context.Context, class_id uuid.UUID, offset int32) ([]domain.Classwork, error) {
+	class_works, err := cw.store.ListClassworkAdmin(c, postgresql.ListClassworkAdminParams{
+		ClassID: class_id,
+		Offset:  offset,
+	})
+	return class_works, err
 }
-func (cw *classworkRepository) Upload(c context.Context, buffer []byte) error {
+
+// TODO!: implement upload method
+func (cw *classworkRepository) Upload(c context.Context, data bytes.Buffer) error {
 	return nil
 }
-func (cw *classworkRepository) Delete(c context.Context, id domain.AssetID) error {
-	return nil
+
+func (cw *classworkRepository) Delete(c context.Context, id uuid.UUID) (*string, error) {
+	class_work, err := cw.store.DeleteClassworkFromClass(c, postgresql.DeleteClassworkFromClassParams{
+		ID: id,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	asset, err := cw.storage.DeleteAsset(c, class_work.Url)
+	if err != nil {
+		return nil, err
+	}
+	deleted_asset := asset.(admin.AssetResult)
+
+	return &deleted_asset.AssetID, nil
 }
