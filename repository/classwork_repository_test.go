b154431f37b2ef21602945f9efd470e672522681
mystique09 <knightkrypto7@gmail.com>
2023-02-10 12:08:05 +0800

    todo: unit test

---
+++ repository/classwork_repository_test.go
@@ -0,0 +1,14 @@
+package repository
+
+import "testing"
+
+// TODO!:
+// testing file upload using postman is too verbous, we can use local files to upload files
+
+func Test_get_asset(t *testing.T) {}
+
+func Test_get_assets(t *testing.T) {}
+
+func Test_upload_asset(t *testing.T) {}
+
+func Test_delete_asset(t *testing.T) {}
