8d7c632a467416cd0c3df8bfcc77d27dc2a05be0
mystique09 <knightkrypto7@gmail.com>
2023-03-14 14:48:23 +0800

    feat: template renderer

--- ui/ui.go
+++ ui/ui.go
@@ -1,6 +1,30 @@
 package ui
 
-import "embed"
+import (
+	"html/template"
+	"io"
 
-//go:embed
-var uiFS embed.FS
+	"github.com/labstack/echo/v4"
+)
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplateRenderer(path string) *TemplateRenderer {
+	return &TemplateRenderer {
+      templates: template.Must(template.ParseGlob(path)),
+  }
+}
